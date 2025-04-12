e5aa80b27eebc32700e0515ef6b13b4595211b58
ParkerH27 <hasenkap@student.psdr3.org>
2025-04-12 17:34:09 -0500

    Update Content Security Policy and change server URL to production

--- src/js/store.js
+++ src/js/store.js
@@ -5,7 +5,7 @@ const store = createStore({
   state: {
     token: null,
     username: null,
-    server: "http://127.0.0.1:8000",
+    server: "https://base.petalcat.dev",
     feed: [],
   },
   actions: {
